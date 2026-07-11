e6c1fdc1af3ac6fbbfe1a55d22182934e268a06a
kartik raner <kartikraner06@gmail.com>
2026-07-11 16:01:00 +0530

    ADD

--- array/insertion.py
+++ array/insertion.py
@@ -1,3 +1,4 @@
+#array insertion
 import array
 
 my_array1 = array.array('i',[1,2,3,4,5])
@@ -27,3 +28,22 @@ def accesElement(array,index):#o(1)
     else:
         print(array[index])
 accesElement(arr1,6) # time complexity  O(1)
+
+
+
+# searching an element in an array
+import array 
+
+my_array1 = array.array('i',[1,2,3,4,5])
+def linear_search(arr,target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+print(linear_search(my_array1,3))    # time complexity O(n)
+
+from array import *
+arr1 = array('i',[1,2,3,4,5])
+arr1.remove(3) # time complexity O(n)
+print(arr1)
+
