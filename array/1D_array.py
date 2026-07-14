31a03f8399a86b83f6e471efc39574f84a6675d2
kartik raner <kartikraner06@gmail.com>
2026-07-14 15:02:08 +0530

    ADA

---
+++ array/1D_array.py
@@ -0,0 +1,66 @@
+#Created by Elshad Karimov on 04/04/2020.
+from array import *
+
+
+4 #1. Create an array and traverse.
+my_array = array('i',[1,2,3,4,5])
+for i in my_array:
+    print(i)    
+
+5 #2. Access individual elements through indexes
+print("step 2")
+print(my_array[0])
+print(my_array[3])
+#3. Append any value to the array using append() method
+print("step 3")
+my_array.append(6)
+print(my_array  )
+#4. Insert value in an array using insert() method
+print("step 4")
+my_array.insert(0,11)
+print(my_array)
+8 #5. Extend python array using extend() method
+print("step 5")
+my_array1 = array('i',[7,8,9])
+my_array.extend(my_array1)
+print(my_array)
+#6. Add items from list into array using fromlist() method
+print("step 6")
+temp_list = [10,11,12]
+my_array.fromlist(temp_list)
+print(my_array)
+#7. Remove any array element using remove() method
+print("step 7")
+my_array.remove(11)
+print(my_array)
+
+
+11 #8. Remove last array element using pop() method
+
+12
+
+#9. Fetch any element through its index using index() method
+
+13 #10. Reverse a python array using reverse() method
+
+14
+
+#11. Get array buffer information through buffer_info() method
+
+15
+
+#12. Check for number of occurrences of an element using count() method
+
+16 # 13. Convert array to string using tostring() method
+
+17
+
+# 14. Convert array to a python list with same elements using tolist() method
+
+18
+
+#15. Append a string to char array using fromstring() method
+
+19
+
+#16. Slice Elements from an array
