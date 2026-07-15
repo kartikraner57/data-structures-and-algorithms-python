eed92ed6c43503efc10f29496897da02a5690d3e
kartik raner <kartikraner06@gmail.com>
2026-07-15 23:22:50 +0530

    AD

---
+++ array/2D_array.py
@@ -0,0 +1,8 @@
+#day 1 - 11,15,10,6
+#day 2 - 10,14,11,5
+#day 2 - 12,17,12,8
+#day 4 - 15,18,14,9
+
+import numpy as np
+twoDArray = np.array([11,15,10,6,10,14,11,5,12,17,12,8,15,18,14,9])
+print(twoDArray)
