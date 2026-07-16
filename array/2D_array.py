14c96c5fb6cf1db310b453900c40fa1654ca869c
kartik raner <kartikraner06@gmail.com>
2026-07-16 14:18:36 +0530

    AD

--- array/2D_array.py
+++ array/2D_array.py
@@ -4,5 +4,19 @@
 #day 4 - 15,18,14,9
 
 import numpy as np
-twoDArray = np.array([11,15,10,6,10,14,11,5,12,17,12,8,15,18,14,9])
+
+twoDArray = np.array([[11,15,10,6],[10,14,11,5],[12,17,12,8],[15,18,14,9]])
 print(twoDArray)
+newTowDArray = np.insert(twoDArray, 0, [[13,16,13,7]], axis=0) # time complexity O(n)
+print(newTowDArray)
+
+newTowDArray = np.insert(twoDArray, 0, [[13,16,13,7]], axis=1) # time complexity O(n)
+print(newTowDArray)
+
+def accessElement(array, rowIndex, colIndex):
+    if rowIndex >= len(array) and colIndex >= len(array[0]):
+        print('incorrect index')
+    else:
+        print(array[rowIndex][colIndex])
+
+accessElement(twoDArray, 1, 2) # time complexity O(1)
