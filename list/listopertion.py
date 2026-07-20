14a3fe6867264cbecc9a78c5b83d580a3357b17e
kartik raner <kartikraner06@gmail.com>
2026-07-20 21:42:40 +0530

    AD

---
+++ list/listopertion.py
@@ -0,0 +1,42 @@
+# # list operation and funtion
+# a = [1,2,3]
+# b = [4,5,6]
+# c = a + b
+# print(c)
+
+# #
+# a = [0,1]
+# a = a * 4
+# print(a)
+
+# a = [0,1,2,3,4,5,6,7,8,9]
+# print(len(a))
+# print(max(a))
+# print(min(a))
+# print(sum(a))
+# print(sum(a)/len(a))
+
+# #)
+# total = 0
+# count = 0
+# while (True):
+#     inp = input('Enter a number: ')
+#     if inp == 'done': break
+#     value = float(inp)
+#     total = total + value
+#     count = count + 1
+# average = total / count
+# print('Average:', average)
+
+#
+my_List = list()
+total = 0
+count = 0
+while (True):
+    inp = input('Enter a number: ')
+    if inp == 'done': break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+average = total / count
+print('Average:', average)
---
+++ list/serch.py
@@ -0,0 +1,8 @@
+# searching for an element in tg    
+my_list =[10,20,30,40,50,60,70,80,90,100]
+#in operator
+target  = 50
+if target in my_list:
+    print(f"{target} is inthe list")
+else:
+    print(f"{target} is not in the list")
