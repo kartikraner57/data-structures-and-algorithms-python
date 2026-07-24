9cdc817e36c9f4cf7a4efa2d7d7359f3f6b946e1
kartik raner <kartikraner06@gmail.com>
2026-07-24 11:01:51 +0530

    AD

--- list/serch.py
+++ list/serch.py
@@ -6,3 +6,13 @@ if target in my_list:
     print(f"{target} is inthe list")
 else:
     print(f"{target} is not in the list")
+
+
+# # Linear search
+# def linear_search(p_list, p_target):
+#     for i, value in enumerate(p_list):
+#         if value == p_target:
+#             return i
+        
+#     return -1
+# print(linear_search(my_list, target))
